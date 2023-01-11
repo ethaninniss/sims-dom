a00c23cdc78087c32ff791fd32198269a0e18e3b
ethaninniss <ethan.inniss@gmail.com>
2023-01-11 20:25:29 +0000

    added onclick feature for methods

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// SIm Class
+// let firstName = document.getElementById("firstName").value;
+// let lastName = document.getElementById("lastName").value;
+// let gender = document.getElementById("gender").value;
+// let age = document.getElementById("age").value;
+
+// const sims = [];
+let sim;
+
+// Sim Class
 class Sim {
-    constructor(firstName, lastName, gender, age){
+    constructor(firstName, lastName, gender, age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
@@ -17,4 +25,37 @@ class Sim {
         return `My age is ${this.age}`;
     }
 }
+
+const newSim = function() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const gender = document.getElementById("gender").value;
+    const age = document.getElementById("age").value;
+    sim = new Sim(firstName, lastName, gender, age);
+    console.log(sim);
+    // sims.push(new Sim(firstName, lastName, gender, age));
+};
+
+const speech = function(phrase) {
+    const textBox = document.getElementById("text-box");
+    let text = document.createElement("p");
+    text.innerHTML = phrase;
+    text.setAttribute("class", "phrase");
+    textBox.appendChild(text);
+};
+
+document.getElementById("sayNameButton").onclick = function() {
+    console.log(sim.sayName());
+    speech(sim.sayName());
+};
+
+document.getElementById("sayGenderButton").onclick = function() {
+    console.log(sim.sayGender());
+    speech(sim.sayGender());
+};
+
+document.getElementById("sayAgeButton").onclick = function() {
+    console.log(sim.sayAge());
+    speech(sim.sayAge());
+};
 
